0c929c34464ea270e6f48e1c11124414f5fe1c40
SirDuckly <77992530+SirDuckly@users.noreply.github.com>
2024-01-06 20:04:53 +0000

    Update for PC

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -2,13 +2,10 @@ function toggleMenu() {
   //var burgerMenu = document.getElementById('nav-area-burger-click');
   //nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
   var navItemsArea = document.getElementById('nav-area-burger-links-area');
+  var html = document.getElementsByTagName('html');
   if (navItemsArea.style.transform === 'translateY(0%)') {
     navItemsArea.style.transform = 'translateY(-100%)';
   } else {
     navItemsArea.style.transform = 'translateY(0%)';
   }
 }
-
-// Need to add:
-// - Remove burger when displayed
-// - Replace with X svg
